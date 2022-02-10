a2ae9a87448bc2807f9215966014b4f80bdd0c39
Leila Yousefi <leilayousefi@LeilaMac.local>
2022-02-10 14:55:46 +0000

    Added raw-data folder and csv file inside it

---
+++ data-raw/create_regreg.R
@@ -0,0 +1,19 @@
+# some code to go from raw data csv to a nice RDA object
+# create_regreg
+
+regreg <- readr::read_csv("./data-raw/register.csv")
+
+# as factor
+regreg$phase <- forcats::as_factor(regreg$phase)
+
+#give nice name
+regreg <- dplyr::rename(regreg, date = `entry-timestamp`)
+
+# copyright looks empty
+regreg <- dplyr::select(regreg, -copyright)
+
+# overwrite old data
+devtools::use_data(regreg, overwrite = TRUE)
+
+rm(regreg)
+
